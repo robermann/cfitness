277aee93d94adb879e74b34e10fc4bccf7880ecd
eudoxos <github@doxos.eu>
2024-10-27 20:15:45 +0000

    Deploying to gh-pages from @ edhamma/cfitness@3f7e9ee0c6183a76fa36be058bb967bd448339ec 🚀

---
+++ html/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3f7e9ee',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
